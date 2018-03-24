ce340670347268640f20230de7cbde8669ca5dbc
animalart <patxi.luzu@gmail.com>
2018-03-24 23:18:06 +0100

    start button, navigation etc

---
+++ client/elements/buttons/StartButton.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { TouchableHighlight, Text, View, StyleSheet } from 'react-native';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+
+export default class StartButton extends Component{
+
+  render(){
+    return(
+      <View style={{flex:8}}>
+        <TouchableHighlight
+          underlayColor="#4189fc"
+          onPress={this.props.onPress}
+          style={styles.circleButton}>
+            <Text style={styles.text}>Start</Text>
+        </TouchableHighlight>
+    </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  circleButton: {
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#1c73ff',
+    borderRadius:50,
+    height: 100,
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+})
