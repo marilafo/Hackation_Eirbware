ad1e00c2dd1404f11e8e34fc3fa0fe310234e3a1
BAYLE Alexis <alexis.bayle@enseirb-matmeca.fr>
2018-03-25 01:09:23 +0100

    add app

---
+++ app.py
@@ -0,0 +1,43 @@
+import apiai
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    speech = "coucou toi"
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
